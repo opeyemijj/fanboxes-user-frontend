d59d35c9de59662a1ca3a958c603e5d81df40e59
Victor <victorclifford29@gmail.com>
2025-08-25 19:27:54 +0100

    - last fixes

--- components/ToastProvider.jsx
+++ components/ToastProvider.jsx
@@ -12,42 +12,7 @@ export function ToastProvider() {
       visibleToasts={5}
       duration={4000}
       toastOptions={{
-        style: {
-          background: "#fff",
-          color: "#000",
-          border: "1px solid #e5e5e5",
-          boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
-        },
-
-        success: {
-          style: {
-            background: "#f0fdf4",
-            color: "#166534",
-            border: "1px solid #11F2EB",
-          },
-          iconTheme: {
-            primary: "#11F2EB",
-            secondary: "white",
-          },
-        },
-        error: {
-          style: {
-            background: "#fef2f2",
-            color: "#dc2626",
-            border: "1px solid #fecaca",
-          },
-          iconTheme: {
-            primary: "#dc2626",
-            secondary: "white",
-          },
-        },
-        loading: {
-          style: {
-            background: "#fffbeb",
-            color: "#d97706",
-            border: "1px solid #fde68a",
-          },
-        },
+        className: "custom-sonner-toast",
       }}
     />
   );
