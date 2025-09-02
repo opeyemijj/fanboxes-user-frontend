47940c47af1923e6272f9c9362b365da8c492748
Victor <victorclifford29@gmail.com>
2025-09-02 07:49:42 +0100

    - Fixed reduxx causing page break on hard page reload

--- components/_main/LatestBoxes/index.jsx
+++ components/_main/LatestBoxes/index.jsx
@@ -9,7 +9,7 @@ import { setProducts } from "@/redux/slices/product";
 export default function LatestBoxes({ loading = false }) {
   const dispatch = useDispatch();
   const {
-    products,
+    products = [],
     loading: productsLoading,
     error,
   } = useSelector((state) => state.product);
@@ -33,9 +33,10 @@ export default function LatestBoxes({ loading = false }) {
     <section>
       <h2 className="text-3xl font-bold mb-6">Latest boxes</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
-        {latestBoxes.map((box) => (
-          <BoxCard key={box._id} box={box} isNew={true} />
-        ))}
+        {latestBoxes &&
+          latestBoxes.map((box) => (
+            <BoxCard key={box._id} box={box} isNew={true} />
+          ))}
       </div>
     </section>
   );
