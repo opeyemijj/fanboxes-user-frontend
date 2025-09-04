496499abad476a56dec407fcb72d9186bc411e2a
Victor <victorclifford29@gmail.com>
2025-09-04 10:19:03 +0100

    - fixes

--- components/_main/LatestBoxes/index.jsx
+++ components/_main/LatestBoxes/index.jsx
@@ -27,16 +27,16 @@ export default function LatestBoxes({ loading = false, products = [], error }) {
     return <LatestBoxesSkeleton />;
   }
 
-  const latestBoxes = products.slice(0, 9);
+  // const latestBoxes = products.slice(0, 9);
 
   return (
     <section>
       <h2 className="text-3xl font-bold mb-6">Latest boxes</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
-        {latestBoxes?.length ? (
-          latestBoxes.map((box) => (
-            <BoxCard key={box._id} box={box} isNew={true} />
-          ))
+        {products?.length ? (
+          [...products]
+            .slice(0, 9)
+            .map((box) => <BoxCard key={box._id} box={box} isNew={true} />)
         ) : (
           <>
             <h6 className="text-1xl text-gray-500 mb-2">
